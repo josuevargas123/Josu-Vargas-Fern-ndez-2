0cb67340ef01bb2c1202b12eafe11072fddae5e8
JosueVargas5 <90123958+josuevargas123@users.noreply.github.com>
2021-11-27 04:30:07 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+
+int casa=0;
+int carro=0;
+
+cout<< "ingrese numero\n";
+cin>>casa; 
+cout<< "ingrese numero\n" ;
+cin>>carro;
+ if (casa<carro) 
+ ;
+
+
+cout<<casa << "es mayor" << carro;
+
+cout<<casa <<"es menor"<< carro;
+}
